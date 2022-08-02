c4ef5b1251616317f2de17a341ec9db2eaa20764
DaffaPoseidon <ndap.cahyo@gmail.com>
2022-08-02 20:40:15 +0700

    Commit

---
+++ movie-journal/src/Search.svelte
@@ -0,0 +1,35 @@
+<script>
+
+    import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher()
+
+    let searchTerm =''
+
+    const search = (e) => {
+        searchTerm = e.target.value
+        if(searchTerm){
+            dispatch('search', {
+                searchTerm,
+            })
+        }
+
+        else{
+            dispatch('clearSearch', {
+                
+            })
+        }
+    }
+</script>
+
+
+<input 
+    placeholder="Search Your Movies"
+    value={searchTerm}
+    on:keyup={search}    
+/>
+
+<style>
+    input{
+        width: 100%;
+    }
+</style>
