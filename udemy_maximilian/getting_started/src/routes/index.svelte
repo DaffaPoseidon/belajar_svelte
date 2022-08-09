4064aa35ec2bb0ed7c2c244a40d87c7e74393729
DaffaPoseidon <ndap.cahyo@gmail.com>
2022-08-09 15:55:19 +0700

    Update

---
+++ udemy_maximilian/getting_started/src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+    let count = 0
+
+    const IncraseCount = () => {
+        count += 1
+    }
+</script>
+
+<div class="main">
+    <button on:click={IncraseCount}>Klik aku untuk menambahkan {count}</button>
+</div>
+
+<style>
+    .main{
+        text-align: center;
+    }
+
+    .main button{
+        margin-top: 12em;
+        padding: 1em;
+    }
+</style>
