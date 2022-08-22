e6cd4d7ff74fe5eb90d420af148a6be8b8d77921
DaffaPoseidon <ndap.cahyo@gmail.com>
2022-08-22 16:38:11 +0700

    Update

--- udemy_maximilian/Materi/course_project_first_step/src/routes/index.svelte
+++ udemy_maximilian/Materi/course_project_first_step/src/routes/index.svelte
@@ -1,5 +1,31 @@
 <script>
     import Header from './UI/Header.svelte'
+
+    const meetups = [ 
+        {
+            id: 1,
+            title: "NieR: Automata Guide",
+            subtitle: "Learn playing NieR: Automata",
+            description: "In this guide you will learn how to play NieR: Automata in detail",
+            imageUrl: "https://i.pinimg.com/originals/f6/56/e8/f656e828e9afbfeaa91faac9d05c5cb9.jpg",
+            address: "The Bunker",
+            contactEmail: "YoRHa@gmail.com"
+        },
+        {
+            id: 2,
+            title: "NieR: Replicant Guide",
+            subtitle: "Learn playing NieR: Replicant",
+            description: "In this guide you will learn how to play NieR: Replicant in detail",
+            imageUrl: "https://images2.alphacoders.com/113/thumb-1920-1130315.png",
+            address: "Village",
+            contactEmail: "Shadowlord@gmail.com"
+        }
+    ]
 </script>
 
-<Header/>
+<div class="main">
+    <Header/>
+    
+</div>
+
+<style></style>
