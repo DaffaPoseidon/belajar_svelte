8464bbec1f19e4d99faf2e34a0cf690297e4bb7a
DaffaPoseidon <ndap.cahyo@gmail.com>
2022-08-23 21:25:48 +0700

    Adding new Meetups via Form

---
+++ list_toDo/src/routes/index.svelte
@@ -0,0 +1,31 @@
+<script>
+    let name = "NieR"
+    let count = 0
+
+    $: reactiveName = name.toUpperCase()
+
+    const IncraseCount = () => {
+        count += 1
+    }
+</script>
+
+<div class="main">
+    <p>Namaku adalah {reactiveName}</p>
+    <button on:click={IncraseCount}>Klik aku untuk menambahkan {count}</button>
+</div>
+
+<style>
+    .main{
+        text-align: center;
+    }
+
+    .main p{
+        margin-top: 12em;
+        padding: 1em;
+    }
+
+    .main button{
+        margin-top: 1em;
+        padding: 1em;
+    }
+</style>
--- udemy_maximilian/Materi/course_project_first_step/src/routes/index.svelte
+++ udemy_maximilian/Materi/course_project_first_step/src/routes/index.svelte
@@ -2,7 +2,14 @@
     import Header from './UI/Header.svelte'
     import MeetupGrid from './Meetups/MeetupGrid.svelte'
 
-    const meetups = [ 
+    let title = ""
+    let subtitle = ""
+    let address = ""
+    let email = ""
+    let description = ""
+    let imageUrl = ""
+
+    let meetups = [ 
         {
             id: 1,
             title: "NieR: Automata Guide",
@@ -22,15 +29,56 @@
             contactEmail: "Shadowlord@gmail.com"
         }
     ]
+    
+    const addMeetup = () => {
+        const newMeetup = {
+            id: Math.random().toString(),
+            title: title,
+            subtitle: subtitle,
+            description: description,
+            imageUrl: imageUrl,
+            contactEmail: email,
+            address: address,
+        }
+        meetups = [newMeetup, ...meetups]
+        console.log(meetups)
+    }
 </script>
 
 <div class="main">
     <Header/>
+    <form on:submit|preventDefault="{addMeetup}">
+        <div class="form-control">
+            <label for="title">Title</label>
+            <input type="text" id="title" bind:value="{title}">
+        </div>
+        <div class="form-control">
+            <label for="title">Subtitle</label>
+            <input type="text" id="subtitle" bind:value="{subtitle}">
+        </div>
+        <div class="form-control">
+            <label for="title">Address</label>
+            <input type="text" id="address" bind:value="{address}">
+        </div>
+        <div class="form-control">
+            <label for="title">Image URL</label>
+            <input type="text" id="imageUrl" bind:value="{imageUrl}">
+        </div>
+        <div class="form-control">
+            <label for="title">E-mail</label>
+            <input type="email" id="email" bind:value="{email}">
+        </div>
+        <div class="form-control">
+            <label for="description">Description</label>
+            <textarea rows="3" id="description" bind:value="{description}"/>
+        </div>
+        <button type="submit">Save</button>
+    </form>
     <MeetupGrid meetups={meetups}/>
 </div>
 
 <style>
-    main{
+    .main{
         margin-top: 5rem;
     }
 </style>
