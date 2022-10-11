7071be54cbe87682067c858d168b789c5434e977
DaffaPoseidon <ndap.cahyo@gmail.com>
2022-10-11 14:30:25 +0700

    How to Extract Event Data

--- udemy_maximilian/Materi/6_detailing_components/src/routes/components/Product.svelte
+++ udemy_maximilian/Materi/6_detailing_components/src/routes/components/Product.svelte
@@ -1,18 +1,22 @@
 <script>
     import { createEventDispatcher } from "svelte";
 
-    export let buttonAlert = "BUTTON ALERT DI BAWAH!"
+    export let productTitle
 
     const dispatch = createEventDispatcher();
 
     const onDispatch = () => {
-        dispatch('add-to-cart')
+        dispatch(
+            'add-to-cart', {
+            id: 'p1', 
+            name: 'NieR'
+        })
     }
 
 </script>
 
 <article>
-    <h1>{buttonAlert}</h1>
+    <h1>{productTitle}</h1>
     <button on:click="{onDispatch}">Add to cart</button>
-    <button on:click>Delete</button>
+    <button on:click="{() => {dispatch('delete', 'p1')}}">Delete</button>
 </article>
--- udemy_maximilian/Materi/6_detailing_components/src/routes/index.svelte
+++ udemy_maximilian/Materi/6_detailing_components/src/routes/index.svelte
@@ -1,10 +1,17 @@
 <script>
     import Product from "./components/Product.svelte";
 
+    function addToCart(event){
+        console.log(event)
+    }
 
+    function deleteProduct(event){
+        console.log(event.detail)
+    }
 </script>
 
 <Product
-    on:add-to-cart="{() => alert('Add to cart!')}"
-    on:click="{() => alert('Delete!')}"
+    productTitle="A Book"
+    on:add-to-cart="{addToCart}"
+    on:delete="{deleteProduct}"
 />
